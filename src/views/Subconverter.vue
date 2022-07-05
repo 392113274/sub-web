505df1dfe6b82029a3897664f3a0242ac830aa6d
392113274 <52971348+392113274@users.noreply.github.com>
2022-07-05 17:51:32 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -326,16 +326,7 @@ export default {
         excludeRemarks: "",
         includeRemarks: "",
         filename: "",
-        emoji: false,
-        nodeList: false,
-        extraset: false,
-        sort: false,
-        udp: false,
-        tfo: false,
-        scv: false,
-        fdn: false,
-        appendType: false,
-        insert: false, // 是否插入默认订阅的节点，对应配置项 insert_url
+        udp: false, 
         new_name: true, // 是否使用 Clash 新字段
 
         // tpl 定制功能
